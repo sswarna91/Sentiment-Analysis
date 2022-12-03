0cb129824d0ffcba9d164aa98ec80c935cc61115
sswarna91 <97694285+sswarna91@users.noreply.github.com>
2022-12-03 23:10:33 +0530

    Update search.py

--- search.py
+++ search.py
@@ -1,1 +1,7 @@
 import tweepy
+
+# Add Twitter API key and API secret key
+consumer_key = "aUS4uwAtjHury3W4m63Y4OoPM"
+consumer_secret = "4GREYtGRZA6K8wqQGvYoJd4xHQ04TaozMHOCF5awmQrFImAjOf"
+# for authentication to twitter
+auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
